27c9f1b7b30957616cfb66f421c344348ad084d6
Davor <davor097@gmail.com>
2023-11-14 16:49:35 +0100

    Added date mapper

--- src/main/java/com/davor/msscbrewery/web/mappers/BeerMapper.java
+++ src/main/java/com/davor/msscbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import com.davor.msscbrewery.domain.Beer;
 import com.davor.msscbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
 
 	BeerDto beerToBeerDto(Beer beer);
